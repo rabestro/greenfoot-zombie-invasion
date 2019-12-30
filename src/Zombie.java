38c7201c50bfea267e4f83f660c3b68be490200f
Yegor Chemisov <yegor.chemisov@gmail.com>
2019-12-30 11:40:47 +0200

    Detecting single-object collisions at an offset

--- src/Zombie.java
+++ src/Zombie.java
@@ -1,17 +1,13 @@
-import java.util.*;
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
 
-/**
- * Write a description of class Zombie here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
 public class Zombie extends Actor
 {
     int counter = 0;
     int stationaryX;
     int amplitude;
+    private int step = 4;
     
     protected void addedToWorld(World w) {
         stationaryX = getX();
@@ -35,13 +31,16 @@ public class Zombie extends Actor
         setLocation((int)(stationaryX + amplitude*Math.sin(counter/2)), getY());
     }
     
-    public boolean canMarch() {
-        List<Actor> things = getIntersectingObjects(Actor.class);
-        for( int i = 0; i < things.size(); i++ ) {
-            if( things.get(i).getX() > getX() + 20 ) {
+    private boolean canMarch() {
+        int i=0;
+        while(i<=step) {
+            int front = getImage().getWidth()/2;
+            Actor a = getOneObjectAtOffset(i+front, 0, Actor.class);
+            if( a != null ) {
                 return false;
             }
+            i++;
         }
-        return true; 
+        return true;
     }
 }
